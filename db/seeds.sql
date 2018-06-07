4083eb1c6070f4b3602abe2b525f24bd94a12457
Kyle Souther-Bruno <ksoutherbruno@gmail.com>
2018-06-07 10:20:42 -0500

    Fixed

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,9 @@
 -- Seed Burgers into Database ... No Sesame! (hehehe)
 INSERT INTO burgers(burger_name, devoured, date)
-VALUES ("Big Mac", false, "2018-05-23  04:20:00"),
-  ("Whooper", false, "2018-05-23 5 04:20:01"),
-  ("Cheezburger", false, "2018-05-23  04:20:02"),
-  ("Baconator", false, "2018-05-23  16:20:00"),
-  ("Quarter Pounder", false, "2018-05-23  16:20:01"),
+VALUES ("mac", false, "2018-05-23  04:20:00"),
+  ("Plain Burger", false, "2018-05-23 5 04:20:01"),
+  ("Bacon Cheese Burger", false, "2018-05-23  04:20:02"),
+  ("Double Cheese Burger", false, "2018-05-23  16:20:00"),
+  ("Ribeye", false, "2018-05-23  16:20:01"),
   ("McRoyal", false, "2018-05-23  04:20:20"),
-  ("Double Double", true, "2018-05-23 16:20:16");
+  ("Triple Cheese Burger", true, "2018-05-23 16:20:16");
